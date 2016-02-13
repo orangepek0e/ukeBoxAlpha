517bb7aa5eec3571d758f7c1e268c97f7f0ca7b6
orangepek0e <georgejohnston13@hotmail.com>
2016-02-13 16:35:39 -0400

    at long last, api is functioning, and data is in the database.

--- alphaUkeBox/api/server.js
+++ alphaUkeBox/api/server.js
@@ -6,8 +6,8 @@ var mongoose    = require('mongoose');
 var passport	  = require('passport');
 var config      = require('./config/database'); // get db config file
 var User        = require('./app/models/user'); // get the mongoose model
-var port        = process.env.PORT || 8080;
-var jwt         = require('jwt-simple');
+var port 	      = process.env.PORT || 8080;
+var jwt 			  = require('jwt-simple');
 
 // get our request parameters
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -19,42 +19,91 @@ app.use(morgan('dev'));
 // Use the passport package in our application
 app.use(passport.initialize());
 
-// Route (GET http://localhost:8080)
+// demo Route (GET http://localhost:8080)
 app.get('/', function(req, res) {
   res.send('Hello! The API is at http://localhost:' + port + '/api');
 });
 
-// connect to database
 mongoose.connect(config.database);
 
-// pass passport for configuration
 require('./config/passport')(passport);
 
-// bundle our routes
 var apiRoutes = express.Router();
 
-// create a new user account (POST http://localhost:8080/api/signup)
 apiRoutes.post('/signup', function(req, res) {
   if (!req.body.name || !req.body.password) {
-    res.json({success: false, msg: 'Please pass name and password.'});
+    res.json({succes: false, msg: 'Please pass name and password.'});
   } else {
     var newUser = new User({
       name: req.body.name,
       password: req.body.password
     });
-    // save the user
     newUser.save(function(err) {
       if (err) {
-        return res.json({success: false, msg: 'Username already exists.'});
+        res.json({succes: false, msg: 'Username already exists.'});
+      } else {
+        res.json({succes: true, msg: 'Successful created user!'});
       }
-      res.json({success: true, msg: 'Successful created new user.'});
     });
   }
 });
 
-// connect the api routes under /api/*
+apiRoutes.post('/authenticate', function(req, res) {
+  User.findOne({
+    name: req.body.name
+  }, function(err, user) {
+    if (err) throw err;
+
+    if (!user) {
+      res.send({success: false, msg: 'Authentication failed. User not found.'});
+    } else {
+      user.comparePassword(req.body.password, function(err, isMatch) {
+        if (isMatch && !err) {
+          var token = jwt.encode(user, config.secret);
+          res.json({success: true, token: 'JWT ' + token});
+        } else {
+          res.send({success: false, msg: 'Authentication failed. Wrong password.'});
+        }
+      });
+    }
+  });
+});
+
+apiRoutes.get('/memberinfo', passport.authenticate('jwt', {session: false}), function(req, res) {
+  var token = getToken(req.headers);
+  if (token) {
+    var decoded = jwt.decode(token, config.secret);
+    User.findOne({
+      name: decoded.name
+    }, function(err, user) {
+      if (err) throw err;
+
+      if (!user) {
+        return res.status(403).send({success: false, msg: 'Authentication failed. User not found.'});
+      } else {
+        return res.json({success: true, msg: 'Congrats on logging in successfully, ' + user.name + '!'});
+      }
+    });
+  } else {
+    return res.status(403).send({success: false, msg: 'No token provided.'});
+  }
+});
+
+getToken = function(headers) {
+  if (headers && headers.authorization) {
+    var parted = headers.authorization.split(' ');
+    if (parted.length === 2) {
+      return parted[1];
+    } else {
+      return null;
+    }
+  } else {
+    return null;
+  }
+};
+
 app.use('/api', apiRoutes);
 
 // Start the server
 app.listen(port);
-console.log('The magic will happen at: http://localhost:' + port);
+console.log('Go get yourself a cup of tea and check out: http://localhost:' + port);
